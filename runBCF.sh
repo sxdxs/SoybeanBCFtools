00ec08b63fc55a015327b4f20695dfccd7d4ae4a
Wooxie$tim <45835307+sxdxs@users.noreply.github.com>
2022-03-01 15:14:18 -0500

    Create runBCF.sh
    After index, run this. Do not need to include the .csi file generated in command line of runBCF.sh

---
+++ runBCF.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=j_BCFtools
+#SBATCH --partition=batch
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=username@uga.edu
+#SBATCH --ntasks=1
+#SBATCH --mem=10gb
+#SBATCH --time=08:00:00
+#SBATCH --output=BCFtools.%j.out
+#SBATCH --error=BCFtools.%j.err
+cd $SLURM_SUBMIT_DIR
+ml BCFtools/1.6-foss-2019b
+bcftools view --samples-file samplelist.txt soysnp50k_wm82.a2_41317.vcf.gz  > output.vcf
